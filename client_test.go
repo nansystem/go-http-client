9f3471164ba4c2cc7dd75a909d3da63ac1da9869
nansystem <sato171786@gmail.com>
2021-03-20 18:10:56 +0900

    feat: query

--- client_test.go
+++ client_test.go
@@ -3,13 +3,31 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
 func TestGet(t *testing.T) {
-	url := "http://localhost:3000/posts"
+	u := "http://localhost:3000/post"
 
-	resp, err := http.Get(url)
+	resp, err := http.Get(u)
+	if err != nil {
+		t.Fatal("request failed")
+	}
+	// 関数を抜ける際に必ずresponseをcloseするようにdeferでcloseを呼ぶ
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	t.Log(string(byteArray))
+}
+
+func TestGetQueries(t *testing.T) {
+	u := "http://localhost:3000/posts"
+	v := url.Values{}
+	v.Add("title", "json-server")
+	v.Add("author", "typicode")
+
+	resp, err := http.Get(u + "?" + v.Encode())
 	if err != nil {
 		t.Fatal("request failed")
 	}
